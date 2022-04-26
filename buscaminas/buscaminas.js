0c0c8cfd7ab33018a9efad1548cffe4b01334ef8
fizdalf <fizdalf@gmail.com>
2022-04-26 19:05:13 +0100

    refactor and improve code

--- buscaminas/buscaminas.js
+++ buscaminas/buscaminas.js
@@ -1,4 +1,4 @@
-import {Tile, tileStates} from "./tile.js";
+import {TilesManager} from "./tilesManager.js";
 
 export const gameStates = {
     PLAYING: 0,
@@ -7,109 +7,42 @@ export const gameStates = {
 }
 
 export class Buscaminas {
-    _gameState = gameStates.PLAYING;
-    _tiles;
+    #gameState = gameStates.PLAYING;
+    /** @member {TilesManager} */
+    #tilesManager;
 
     constructor(mines) {
-        this._generateTiles(mines);
-    }
-
-    _checkValidMines(mines) {
-        let beforeMine = undefined;
-        for (const mine of mines) {
-            if (beforeMine !== undefined && mine !== beforeMine) {
-                return true;
-            }
-            beforeMine = mine;
-        }
-        return false;
+        this.#tilesManager = new TilesManager(mines);
     }
 
     updateGameStatus(wasMine) {
         if (wasMine) {
-            this._gameState = gameStates.LOST;
+            this.#gameState = gameStates.LOST;
             return;
         }
-        if(this._areThereClosedTilesWithoutMines()){
+        if (this.#tilesManager.areThereClosedTilesWithoutMines()) {
             return;
         }
-        this._gameState = gameStates.WON;
+        this.#gameState = gameStates.WON;
     }
 
     openTile(tile) {
-        const wasMine = this._openTile(tile);
+        const wasMine = this.#tilesManager.openTile(tile);
         this.updateGameStatus(wasMine);
     }
 
     markAndUnmarkTile(tile) {
-        if (this._gameState === gameStates.LOST) {
+        if (this.#gameState === gameStates.LOST) {
             return;
         }
-        this._toggleMarked(tile);
+        this.#tilesManager.toggleMarked(tile);
     }
 
     gameState() {
-        return this._gameState;
+        return this.#gameState;
     }
 
     tileState() {
-        return this._tileState();
-    }
-
-    _areThereClosedTilesWithoutMines(){
-        for (const tile of this._tiles){
-            if (tile.state() === tileStates.CLOSED && !tile.hasMine()) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    _sideTile(numberTile) {
-        if (this._tiles.length - 1 < numberTile + 1) {
-            return numberTile - 1;
-        } else {
-            return numberTile + 1;
-        }
-    }
-
-    _tileState(){
-        let result;
-        result = this._tiles.map((tile) => {
-            return tile.state()
-        });
-        return result;
-    }
-
-    _toggleMarked(tile) {
-        return this._tiles[tile].toggleMarked();
-    }
-
-    _generateTiles(mines) {
-        if (!this._checkValidMines(mines)) {
-            throw new Error("The board has to have at least one mine, and at least one empty tile");
-        }
-        this._tiles = mines.map((mine) => {
-            return new Tile(mine)
-        });
-    }
-
-    _openTile(tile) {
-        const wasMine = this._tiles[tile].openTile();
-        if (wasMine) {
-            return true;
-        }
-        this._openNeighborTiles(tile);
-        return false;
-    }
-
-    _hasMine(tile) {
-        return this._tiles[tile].hasMine()
-    }
-
-    _openNeighborTiles(tile) {
-        if (!this._hasMine(this._sideTile(tile))) {
-            this._tiles[this._sideTile(tile)].openTile();
-        }
+        return this.#tilesManager.tileState();
     }
 }
---
+++ buscaminas/tilesManager.js
@@ -0,0 +1,77 @@
+import {Tile, tileStates} from "./tile.js";
+
+export class TilesManager {
+    #tiles
+
+    constructor(mines) {
+        this.#generateTiles(mines);
+    }
+
+    #checkValidMines(mines) {
+        let beforeMine = undefined;
+        for (const mine of mines) {
+            if (beforeMine !== undefined && mine !== beforeMine) {
+                return true;
+            }
+            beforeMine = mine;
+        }
+        return false;
+    }
+
+    areThereClosedTilesWithoutMines() {
+        for (const tile of this.#tiles) {
+            if (tile.state() === tileStates.CLOSED && !tile.hasMine()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    #sideTile(numberTile) {
+        if (this.#tiles.length - 1 < numberTile + 1) {
+            return numberTile - 1;
+        } else {
+            return numberTile + 1;
+        }
+    }
+
+    tileState() {
+        let result;
+        result = this.#tiles.map((tile) => {
+            return tile.state()
+        });
+        return result;
+    }
+
+    toggleMarked(tile) {
+        return this.#tiles[tile].toggleMarked();
+    }
+
+    #generateTiles(mines) {
+        if (!this.#checkValidMines(mines)) {
+            throw new Error("The board has to have at least one mine, and at least one empty tile");
+        }
+        this.#tiles = mines.map((mine) => {
+            return new Tile(mine)
+        });
+    }
+
+    openTile(tile) {
+        const wasMine = this.#tiles[tile].openTile();
+        if (wasMine) {
+            return true;
+        }
+        this.#openNeighborTiles(tile);
+        return false;
+    }
+
+    #hasMine(tile) {
+        return this.#tiles[tile].hasMine()
+    }
+
+    #openNeighborTiles(tile) {
+        if (!this.#hasMine(this.#sideTile(tile))) {
+            this.#tiles[this.#sideTile(tile)].openTile();
+        }
+    }
+}
