19aaae67f4309f9de54268edd6614ad03afbedc0
rayco <rayco7958@gmail.com>
2022-05-18 13:58:44 +0200

    listas enlazadas

---
+++ data-structures/lista enlazada/lista enlazada.spec.js
@@ -0,0 +1,21 @@
+class Node{
+    data;
+    next;
+    constructor(data) {
+        this.data = data;
+    }
+}
+
+describe("Nodes", () => {
+    it('should return the first data node', function () {
+        const node = new Node(1)
+        expect(node.data).toBe(1)
+    });
+
+    it("should return the second data node", function () {
+        const node = new Node(17)
+        node.next = new Node(37)
+        expect(node.next.data).toBe(37)
+    })
+})
+
