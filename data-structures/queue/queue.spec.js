fcce794cf05433960dff91aa2260b036ad95f543
rayco <rayco7958@gmail.com>
2022-05-18 11:48:39 +0200

    queue terminado

--- data-structures/queue/queue.spec.js
+++ data-structures/queue/queue.spec.js
@@ -2,26 +2,37 @@
 // FIFO (First In First Out)
 
 class Queue {
-    queue;
+    queue = [];
     push(item1) {
-        this.queue = item1
+        this.queue.push(item1);
     }
 
     get() {
-        return this.queue;
+        return this.queue.shift();
     }
-
 }
-
 describe('Queue', () => {
-    it("should return one when one is the first item", () => {
+    it.each([
+        [1]
+        [2]
+    ])("should return the only item", (item) => {
         const queue = new Queue();
-        queue.push(1);
-        expect(queue.get()).toBe(1)
+        queue.push(item);
+        expect(queue.get()).toBe(item)
     });
-    it("should return two when two is the first item", () => {
+
+    it("should return only the first item", () => {
         const queue = new Queue();
+        queue.push(7);
         queue.push(2);
+        expect(queue.get()).toBe(7)
+    });
+
+    it("should return the second item after return the first item", () => {
+        const queue = new Queue();
+        queue.push(7);
+        queue.push(2);
+        queue.get();
         expect(queue.get()).toBe(2)
     });
 });
